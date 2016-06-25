9f281f481e045deb06c44c0ef095a57104fd4ae5
James Kluvo <jameskluvo@gmail.com>
2016-06-25 16:38:43 -0700

    assignment 13 arrays 1st commit - not completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,104 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
+    
+    
+    // String to Array
+    // there is a string that contains the names of some characters - characterString
+    // we want to convert that string into an array - arrayOfStarTrekCharactersFromString
+    
+    //characters: worf, picard, crusher
+    
+    NSString *stringOfCharacters = @"worf;picard;crusher";
+    NSArray *arrayOfCharacters = [stringOfCharacters componentsSeparatedByString:@";"];
+    [arrayOfCharacters enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+     //Not sure what to put here
+    }];
+    
+    
+    
     return @[];
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    //Array to a String
+    
+    // characterArray contains the name of some characters
+    // we want to convert that array into a string - stringOfStarTrekCharactersFromArray
+    
+    NSMutableArray *arrayOfCharacters = [@[@"worf",
+                                         @"picard",
+                                           @"crusher"] mutableCopy];
+    NSString *stringOfCharacters = [arrayOfCharacters componentsJoinedByString:@";"];
+    
+    
+    return stringOfCharacters;
+    
+    //is this all good...?
+    
+    
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
+    
+    //Array to Array
+    //Taking in characterArray, putting out the characters in that array sorted Alphabetically as alphabeticallySortedStarTrekCharactersFromArray
+    
+    NSMutableArray *alphabeticallySortedCharacters = [@[@"worf",
+                                                        @"picard",
+                                                        @"crusher"] mutableCopy];
+    NSSortDescriptor *alphaSortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [alphabeticallySortedCharacters sortUsingDescriptors:@[alphaSortDescriptor]];
+    
+    
+    //??? Not sure what to put here or if return @[]; is okay ???
+    
+    
+    
     return @[];
 }
 
-- (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
+
+
+
+
+
+- (BOOL) characterArrayContainsWorf:(NSMutableArray *)characterArray { //I changed characterArray from NSArray to NSMutableArray
     /* WORK HERE */
+    
+    
+    
+    //Looks at the array characterArray to see if "Worf" is in it
+    //Return the BOOL characterArrayContainsWorf as YES or NO
+    
+    
+    //Would removing [c] cause it to search for all forms of worf? ex: Worf, WORF, wOrF etc ???
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    
+    [characterArray filterUsingPredicate:containsWorf];
+    
+    // ??? Do I need anything else???
+    
     return NO;
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
