e3fd1fd5feabc031eb7032d35705455f116564fd
James Kluvo <jameskluvo@gmail.com>
2016-06-25 09:52:44 -0700

    assignment-11-if-else final submission

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,20 +15,66 @@
 
     /* WORK HERE */
     
-    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
-    return itemToReturn;
+    //take in a the NSInteger dollars
+    //put out any items mary could purchase NSString *itemToReturn
+    
+    
+    //if itemMaryCanPurchaseForDollars <= dollars then return the NSString itemToReturn
+    //else return NSString Mary Cant Afford this
+    
+    NSString *returnString;
+    
+    if (dollars == 4) {
+        returnString = @"get out of my store";
+    } else if (dollars == 5){
+        returnString = @"have some gum";
+    } else if (dollars == 6) {
+        returnString = @"have an apple";
+    } else if (dollars == 1000) {
+        returnString = @"have an Apple computer";
+    } else if (dollars == 1000000000) {
+        returnString = @"have The Big Apple";
+    }
+    
+    return returnString;
 }
+
+
+
+
+
+
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
     /* WORK HERE */
-
-    NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
     
-    return cost;
+//    
+   NSUInteger cost = 24;
+//    
+//    
+//    
+//    
+//    
+//    if (self.getsDiscount) {
+//        cost *= .75;
+//    }
+//    
+    
+    
+    return (self.getsDiscount) ? cost * .75 : cost;
+    
 }
+
+
+
+
+
+
+
+
+
+
+
 
 @end
