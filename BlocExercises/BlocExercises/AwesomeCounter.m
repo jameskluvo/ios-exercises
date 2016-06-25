1a2224fcc48672d4c5f79ed96dad94a1a3f4f620
James Kluvo <jameskluvo@gmail.com>
2016-06-25 12:01:18 -0700

    assignment-12-loops 1st commit

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,6 +12,23 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
+    
+    //Printing a string of numbers. With the constraits of 2 numbers, a high number and low number
+    //For example: if number is 4 and otherNumber is 10, printed string would be 45678910
+    
+    
+    //take in number and take in otherNumber, print a string of all the numbers in between
+    
+    //NSInteger lowNumber = number;
+    //NSInteger highNumber = otherNumber;
+    
+    //a string containing these numbers below aka stringWithNumbersBetweenNumber
+    
+    for (NSInteger number; number <= otherNumber; number++) {
+        <#statements#>
+    }
+    
+    
     return @"";
 }
 
