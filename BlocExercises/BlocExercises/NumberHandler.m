ecaa133a712b1422b30894462a81078cd960c92b
James Kluvo <jameskluvo@gmail.com>
2016-06-27 21:42:46 -0700

    assignment 15 datatypes 1st commit - not completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,85 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    //takes in the NSNumber number, doubles it, puts it out as the NSNumber numberThatIsTwiceAsBigAsNumber
+    //need to unbox number to multiply it, then rebox it?
+    
+    //unboxing number as an int
+    int doubleNumberToReturn = [number intValue];
+    
+    //doubling
+    doubleNumberToReturn = doubleNumberToReturn * 2;
+    
+    // need to return an NSNumber
+    //rebox (?) the int doubleNumberToReturn as an NSNumber...?
+    NSNumber *boxedDoubleNumber = [NSNumber numberWithInt:doubleNumberToReturn];
+    
+    return boxedDoubleNumber;
+    
+   //return @0;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
+    //similar to the exercise in Loops
+    
+    //takes in the NSInteger number and NSInteger otherNumber
+    //puts out an array of Integers between number and otherNumber
+    //going to need to get the array of integers, then box it up...?
+    
+    
+    
     return @[];
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
+    
+    //unbox arrayOfNumbers, find the lowest,return as an int
+    
+    
+    
     return 0;
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
