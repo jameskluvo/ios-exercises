d74682017fe65c0e632e34766303bfdbfdd08b9d
James Kluvo <jameskluvo@gmail.com>
2016-06-29 12:45:14 -0700

    assignment 14 dictionaries pre meeting commit

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -13,16 +13,17 @@
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
     
-    //Logic: looks through characterDictionary to find the NSString with a characters favorite drink
+    // theory: looks through characterDictionary to find the NSString with a characters favorite drink
+    // I believe this should print: This characters favorite drink (the key) is xx (the object)
+    // for example: This characters "Favorite Drink" is root beer
     
+    // issues:
+    // how do I store it in the return string favoriteDrinkForStarTrekCharacterDictionary... then return it??
     
-    // I think this should print: This characters favorite drink (the key) is xx (the object)
-    //example: This characters "Favorite Drink" is root beer
    [characterDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
        NSLog(@"This characters  \"%@\" is %@", key, obj);
    }];
     
-    // ??? store it in the return string favoriteDrinkForStarTrekCharacterDictionary, then return it ???
     
    
     
@@ -32,12 +33,13 @@
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
     
-    //Logic: takes in the array of characters, which is holding strings of information about them, such as fav drinks
+    //theory: takes in the array of characters, which is holding strings of information about them, such as fav drinks
     //       Returns an array of strings, only containing everyones favorite drink
     
+    //Issue:
+    // how do you return an array ?
     
-    
-    // ??? How does it know to stop on drinks, and print multiple for all of the characters ???
+    // ??? Question for Antonio:  How does it know to stop on drinks, and print multiple for all of the characters ???
     
     [charactersArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         NSLog(@"Everyones favorite drinks are %@", obj);
@@ -51,14 +53,18 @@
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
     
-    //Logic:
+    //theory:
     //  Take characterDictionary, make a muteable copy
     //  add quote key and objects to the copy
+    
+    //issues:
+    //very confused here, I think I just added another key / section to this dictionary ... ?
+
+    
     
     NSMutableDictionary *characterDictionaryWithQuote = [characterDictionary mutableCopy];
     [characterDictionaryWithQuote setObject:@"this is an example quote" forKey:@"quote"];
     
-    //very confused here, I think I just added another key / section to this dictionary ... ?
     
     
     
